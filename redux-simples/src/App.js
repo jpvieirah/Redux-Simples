27a99911b9f79d5e1ee0d2b04039a0f5a081d012
jpvieirah <jp201101@gmail.com>
2022-04-08 11:05:29 -0300

    Componente Card

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <h1>Exercício React-Redux (Simples)</h1>
+      <div className='linha'>
+      <Card title="Card 1" red> x </Card>
+      <Card title="Card 2" green> x </Card>
+
+      </div>
+      <div className='linha'>
+      <Card title="Card 3" blue>  y </Card>
+      <Card title="Card 4" purple>  y </Card>
+
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+//class name linha ajuda a diferenciar o que quero colocar na linha de cima ou de baixo envolvendo numa div classname linha//
