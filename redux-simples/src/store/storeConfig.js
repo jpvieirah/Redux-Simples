316a57b14ad30ca2ab0e213d342170e242c0e990
jpvieirah <jp201101@gmail.com>
2022-04-11 10:15:54 -0300

    Conectando Componentes com Redux #01

--- redux-simples/src/store/storeConfig.js
+++ redux-simples/src/store/storeConfig.js
@@ -3,8 +3,8 @@ import { createStore, combineReducers } from "redux"
 const reducers = combineReducers({
     numeros: function(state, action) {
         return {
-            min: 1,
-            max: 10
+            min: 7,
+            max: 31
         }
     },
     nomes: function(state, action) {
