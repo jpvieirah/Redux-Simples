1b8f8bae54edda2e070d78e504bc3e3a115f617b
jpvieirah <jp201101@gmail.com>
2022-04-12 09:34:35 -0300

    Invocando Action Creator #03

--- redux-simples/src/components/Interval.jsx
+++ redux-simples/src/components/Interval.jsx
@@ -2,7 +2,7 @@ import "./Interval.css"
 import React from "react"
 import Card from './Card' 
 import { connect } from "react-redux"
-import { alterarNumeroMinimo } from "../store/actions/numeros"
+import { alterarNumeroMinimo, alterarNumeroMaximo } from "../store/actions/numeros"
 
 function Intervalo(props) {
     const { min, max } = props
@@ -15,7 +15,7 @@ function Intervalo(props) {
             </span>
             <span>
                 <strong>Máximo: </strong>
-                <input type="number" value={max} readOnly/>
+                <input type="number" value={max} onChange={e => props.alterarMaximo(+e.target.value)}/>
             </span>
             </div>
         </Card>
@@ -36,7 +36,12 @@ function mapDispatchToProps(dispatch) {
        //action creator -> action
    const action = alterarNumeroMinimo(novoNumero)
    dispatch(action)
-   }
+   },
+   alterarMaximo(novoNumero) {
+    //action creator -> action
+const action = alterarNumeroMaximo(novoNumero)
+dispatch(action)
+},
   }
 }
 
--- redux-simples/src/store/actions/numeros.js
+++ redux-simples/src/store/actions/numeros.js
@@ -5,3 +5,11 @@ export function alterarNumeroMinimo(novoNumero) {
         payload: novoNumero
     }
 }
+
+//action creator
+export function alterarNumeroMaximo(novoNumero) {
+    return {
+        type: 'NUM_MAX_ALTERADO',
+        payload: novoNumero
+    }
+}
